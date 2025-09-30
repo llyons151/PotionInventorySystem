6c2737f6b6c065a5fbfcedcfafd03e590c5a6eb8
llyons151 <lukelyons309@gmail.com>
2025-09-30 16:13:17 -0500

    fixed clang for real this time and finished potion.cpp class

--- src/potion.cpp
+++ src/potion.cpp
@@ -1,8 +1,28 @@
 #include "potion.h"
 
-//Constructors
+// Constructors
 Potion::Potion() : szName(""), szType(""), iPotency(0), dQuantity(0.0){};
 Potion::Potion(std::string name, std::string type, int potency, double quantity)
     : szName(name), szType(type), iPotency(potency), dQuantity(quantity){};
 
+// Getter and Setter functions
+void Potion::setName(std::string szName) { this->szName = szName; };
+std::string Potion::getName() { return szName; };
 
+void Potion::setType(std::string szType) { this->szType = szType; };
+std::string Potion::getType() { return szType; };
+
+void Potion::setPotency(int iPotency) { this->iPotency = iPotency; };
+int Potion::getPotency() { return iPotency; };
+
+void Potion::setQuantity(double dQuantity) { this->dQuantity = dQuantity; };
+double Potion::getQuantity() { return dQuantity; };
+
+// Display function
+void Potion::display(){
+    std::cout << "Name: " << szName
+                << "Type: " << szType
+                << "Potency: " << iPotency
+                << "Quantity: " << dQuantity 
+                << std::endl;
+};
