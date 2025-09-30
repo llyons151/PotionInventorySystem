0de86d6f4f04689788c99b880b36500f934c9ff2
llyons151 <lukelyons309@gmail.com>
2025-09-30 15:46:36 -0500

    added file that stopped clang from bitching at me over imports

--- src/potion.cpp
+++ src/potion.cpp
@@ -1,1 +1,8 @@
 #include "potion.h"
+
+//Constructors
+Potion::Potion() : szName(""), szType(""), iPotency(0), dQuantity(0.0){};
+Potion::Potion(std::string name, std::string type, int potency, double quantity)
+    : szName(name), szType(type), iPotency(potency), dQuantity(quantity){};
+
+
